f949a41ff5c9287399d235df641348c0c5d49725
BrandedNomad <info@brandednomad.com>
2019-08-25 15:19:27 +0700

    Updates

---
+++ precache-manifest.74ce0f65c49a549e243bb995c3a4c735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa94e70c8ebd41bf0bd7093b6ec453a6",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "adc0e12397a8b0895eef",
+    "url": "/Ravenous/static/css/main.9c69f9a4.chunk.css"
+  },
+  {
+    "revision": "3ac0d5e4ce08241a6634",
+    "url": "/Ravenous/static/js/2.7536d8be.chunk.js"
+  },
+  {
+    "revision": "adc0e12397a8b0895eef",
+    "url": "/Ravenous/static/js/main.83ba4538.chunk.js"
+  },
+  {
+    "revision": "8f76e8b44bbf5339685e",
+    "url": "/Ravenous/static/js/runtime~main.be067a22.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
